a0209b5274955c98cb0fdc025d3cae5b2caee98d
Matt Trussler <meberem@gmail.com>
2019-01-17 21:30:42 +0000

    Add sample applications

---
+++ example/FrameworkApplication/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExampleProject
+{
+    /// <summary>
+    /// I am the class
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// I am a public member so my xml doc will be preserved
+        /// </summary>
+        public void PublicMember()
+        {
+        }
+
+        /// <summary>
+        /// I am a private member so I will disappear
+        /// </summary>
+        private void PrivateMember()
+        {
+        }
+    }
+}
